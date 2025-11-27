cfa17ff6eaefd60e334128546f448f7acdb71b94
Dufour <161844234+Duofour@users.noreply.github.com>
2025-11-27 17:35:35 +0100

    Initial commit

---
+++ src/main/java/org/example/liegptprojekt/LieGpTprojektApplication.java
@@ -0,0 +1,13 @@
+package org.example.liegptprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LieGpTprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LieGpTprojektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/liegptprojekt/LieGpTprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.liegptprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LieGpTprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
